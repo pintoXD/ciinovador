dfd98f9451280201af04779e0080e52184617a4c
Otto Pinto <ottolopes20@gmail.com>
2024-10-30 17:14:11 -0300

    added verilog tests

---
+++ verilog_tests/arch_fpga_asic/first_xcelium/xcelium.d/run.lnx8664.22.09.d/files.ts
@@ -0,0 +1,4 @@
+1730318954 /home/cinovador/Documents/course_files/verilog_tests/arch_fpga_asic/first_xcelium/andgate_tb.sv
+1730317714 /home/cinovador/Documents/course_files/verilog_tests/arch_fpga_asic/first_xcelium/andgate.sv
+1730319173 /home/cinovador/Documents/course_files/verilog_tests/arch_fpga_asic/first_xcelium/orgate_tb.sv
+1730318984 /home/cinovador/Documents/course_files/verilog_tests/arch_fpga_asic/first_xcelium/orgate.sv
