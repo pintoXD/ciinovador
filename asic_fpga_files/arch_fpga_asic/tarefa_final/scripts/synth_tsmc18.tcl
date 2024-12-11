b8564462d991a70a9f12c0b4ae06f9bd2e38e5ca
Otto Pinto <ottolopes20@gmail.com>
2024-12-11 09:41:09 -0300

    stabel tcl script version

--- asic_fpga_files/arch_fpga_asic/tarefa_final/scripts/synth_tsmc18.tcl
+++ asic_fpga_files/arch_fpga_asic/tarefa_final/scripts/synth_tsmc18.tcl
@@ -1,30 +1,45 @@
 set working_dir $env(WORKING_DIR)
+set source_dir $env(SOURCE_DIR)
+set lib_dir $env(LIB_DIR)
+set logs_dir $env(LOGS_DIR)
 set netlist_dir $env(NETLIST_DIR)
 set reports_dir $env(REPORTS_DIR)
 
-puts "Working directory: $working_dir"
-puts "Netlist directory: $netlist_dir"
-puts "Reports directory: $reports_dir"
+# set tscm18_slow "tsmc18_slow"
+# set gpdk45_slow "gpdk45_slow"
+# set gpdk45_fast "gpdk45_fast"
 
+# puts "Working directory: $working_dir"
+# puts "Library directory: $lib_dir"
+# puts "Netlist directory: $netlist_dir"
+# puts "Reports directory: $reports_dir"
 
-# read_lib library/slow.lib
+puts "Reading libraries and design files..."
+read_lib $lib_dir/slow.lib
+read_hdl -sv $working_dir/source/ula8.sv
 
-# read_hdl source/ula8.sv
+#Elaborate the ULA8 module
+puts "Elaborating the design..."
+elaborate ula8
 
-# elaborate ula8
+#Check for unseresolved references
+puts "Checking for unresolved references..."
+check_design -unresolved
 
-# check_design -unresolved
+#Perform synthesis
+puts "Performing synthesis..."
+syn_gen
+syn_map
+syn_opt
 
-# syn_gen
+#Generate Reports
+puts "Generating reports..."
+report area > $reports_dir/ula8_tsmc18_area.txt
+report power > $reports_dir/ula8_tsmc18_power.txt
+report gates > $reports_dir/ula8_tsmc18_gates.txt
 
-# syn_map
+#Write the netlist
+puts "Writing the netlist..."
+write_hdl > $netlist_dir/ula8_tsmc18_netlist.v
 
-# syn_opt
-
-# report area > reports/ula8_tsmc18_area.txt
-
-# report power > reports/ula8_tsmc18_power.txt
-
-# report gates > reports/ula8_tsmc18_gates.txt
-
-# write_hdl > ula8_tsmc18_netlist.sv
+exit
