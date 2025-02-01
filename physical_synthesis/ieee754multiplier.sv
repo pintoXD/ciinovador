91f15c1fe1532cd039457c95d74e6c89f25b06b3
Otto Pinto <ottolopes20@gmail.com>
2025-02-01 14:50:38 -0300

    one more minimalm version

--- physical_synthesis/ieee754multiplier.sv
+++ physical_synthesis/ieee754multiplier.sv
@@ -1,4 +1,5 @@
 module multiplier (
+    input logic clk, reset,
     input logic [31:0] input_a,
     input logic [31:0] input_b,
     output logic [31:0] result
@@ -12,6 +13,9 @@ logic [47:0] mantissa_temp;
 logic [9:0] exponent_temp;
 
 always_comb begin : blockName
+    mantissa_temp = mantissa_temp; //the result is 0
+    exponent_temp = exponent_temp; //the exponent is 0
+
     signal_a = input_a[31]; //extract the sign bit from input_a
     signal_b = input_b[31]; //extract the sign bit from input_b
 
@@ -23,19 +27,35 @@ always_comb begin : blockName
     mantissa_b[23] = 1;
     mantissa_b[22:0] = input_b[22:0];  //extract the mantissa from input_b adding the implict 1 at the beginning
 
+    if (mantissa_a[22-:23] == 0 || mantissa_b[22-:23] == 0) begin //if any of the mantissas is 0
+        mantissa_temp = 0; //the result is 0
+        exponent_temp = 0; //the exponent is 0
+    end else begin
+        mantissa_temp = mantissa_a * mantissa_b; //multiply the mantissa
+        exponent_temp = exponent_a + exponent_b - 127; //add the exponents and subtract the bias
 
-    mantissa_temp = mantissa_a * mantissa_b; //multiply the mantissa
-    exponent_temp = exponent_a + exponent_b - 127; //add the exponents and subtract the bias
+        if (mantissa_temp[47] == 1) begin //if the most significant bit of the result is 1
+            mantissa_temp = mantissa_temp >> 1; //shift the result to the right
+            exponent_temp = exponent_temp + 1; //increment the exponent
+        end
 
-    if (mantissa_temp[47] == 1) begin //if the most significant bit of the result is 1
-        mantissa_temp = mantissa_temp >> 1; //shift the result to the right
-        exponent_temp = exponent_temp + 1; //increment the exponent
     end
 
-    result[31] = signal_a ^ signal_b; //XOR the sign bits to get the sign of the result
-    result[30:23] =  exponent_temp[7:0]; //add the exponents and subtract the bias
-    result[22:0] = mantissa_temp[45:23]; //extract the 24 most significant bits of the result
+    // result[31] = signal_a ^ signal_b; //XOR the sign bits to get the sign of the result
+    // result[30:23] =  exponent_temp[7:0]; //add the exponents and subtract the bias
+    // result[22:0] = mantissa_temp[45:23]; //extract the 24 most significant bits of the result
 
+end
+
+always_ff @(posedge clk, posedge reset) begin : clocked_block
+
+    if (reset) begin
+        result <= 32'b0;
+    end else begin
+        result[31] <= signal_a ^ signal_b; //XOR the sign bits to get the sign of the result
+        result[30:23] <=  exponent_temp[7:0]; //add the exponents and subtract the bias
+        result[22:0] <= mantissa_temp[45:23]; //extract the 24 most significant bits of the result
+    end
 end
 
 
--- physical_synthesis/ieee754multiplier_tb.sv
+++ physical_synthesis/ieee754multiplier_tb.sv
@@ -1,18 +1,31 @@
+`timescale 1ns/1ps
 module multiplier_tb (
 );
 
 logic [31:0] input_a;
 logic [31:0] input_b;
 logic [31:0] result;
+logic clk, reset;
 
 shortreal mock_a;
 shortreal mock_b;
 
 multiplier DUT (
+    .clk(clk),
+    .reset(reset),
     .input_a(input_a),
     .input_b(input_b),
     .result(result)
 );
+
+initial begin
+    clk = 0;
+    forever begin
+        #1ns;
+        clk = ~clk;
+    end
+end
+
 
 
 initial begin
@@ -23,6 +36,34 @@ initial begin
 
     input_a = $shortrealtobits(mock_a);
     input_b = $shortrealtobits(mock_b);
+
+    #10;
+    $display("Result Bits: %b", result);
+    $display("Result: %f", $bitstoshortreal(result));
+    $display("Expected: %f", mock_a * mock_b);
+
+    // $finish;
+
+    #10;
+
+    mock_a = 3.1415;
+    mock_b = 0.792;
+
+    input_a = $shortrealtobits(mock_a);
+    input_b = $shortrealtobits(mock_b);
+
+    #10;
+    $display("Result Bits: %b", result);
+    $display("Result: %f", $bitstoshortreal(result));
+    $display("Expected: %f", mock_a * mock_b);
+
+    #10;
+
+    mock_a = 0.0;
+    mock_b = 3.1415;
+
+    input_a = $shortrealtobits(mock_a);
+    input_b = 32'b0_11111001_11100000000000000000000;//$shortrealtobits(mock_b);
 
     #10;
     $display("Result Bits: %b", result);
