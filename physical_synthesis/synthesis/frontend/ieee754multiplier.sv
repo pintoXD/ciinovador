0e4e5ef022a2482618723dc42187da292c574ec7
Otto Pinto <ottolopes20@gmail.com>
2025-02-10 16:32:50 -0300

    trying to make this thing work as expected

--- physical_synthesis/synthesis/frontend/ieee754multiplier.sv
+++ physical_synthesis/synthesis/frontend/ieee754multiplier.sv
@@ -1,64 +1,222 @@
 module ieee754multiplier (
-    input logic clk, reset,
-    input logic [31:0] input_a,
-    input logic [31:0] input_b,
-    output logic [31:0] result
+    input logic clk, rst_n,
+    input logic [31:0] a_i,
+    input logic [31:0] b_i,
+    input logic start_i,
+    output logic done_o,
+    output logic nan_o,
+    output logic inifinit_o,
+    output logic overflow_o,
+    output logic underflow_o,
+    output logic [31:0] product_o
 );
     
+
+typedef enum logic[2:0] {  
+    INITIAL_STATE = 3'b000,
+    MULTIPLY_STEP = 3'b001,
+    RESULT_VERIFICATION_STEP = 3'b010,
+    NAN_O_DETECTED = 3'b011,
+    INIFINIT_O_DETECTED = 3'b100,
+    OVERFLOW_O_DETECTED = 3'b101,
+    UNDERFLOW_O_DETECTED = 3'b110,
+    DONE = 3'b111
+
+} MULTIPLIER_FSM_STATE;
+
+/*****************************************
+*
+* control variables
+*
+******************************************/
+DECODER_FSM_STATE current_state, next_state;
+
+
+/*****************************************
+*
+* internal signals
+*
+******************************************/
+
 logic signal_a, signal_b;
+logic is_multiplication_done;
 logic [7:0] exponent_a, exponent_b; //The 24th bit is unused until the multiplication is computed
 logic [23:0] mantissa_a, mantissa_b; //The 24th bit is unused until the multiplication is computed
-logic [22:0] mantissa_result;
+logic [22:0] mantissa_product_o;
 logic [47:0] mantissa_temp;
 logic [9:0] exponent_temp;
 
+
+always_ff @(posedge clk, negedge rst_n) begin : MULTIPLIER_STATE_CHANGER
+    if(!rst_n)
+        current_state <= INITIAL_STATE;
+    else
+        current_state <= next_state;
+end
+
+
+always_comb begin : MULTIPLIER_COMB_BLOCK
+    next_state = current_state;
+
+    if(!rst_n)begin
+        is_multiplication_done = 0;
+    end else begin 
+        case (current_state)
+            INITIAL_STATE:
+                if (start_i) begin
+                    next_state = MULTIPLY_STEP;
+                end 
+
+            MULTIPLY_STEP: begin
+                    mantissa_temp = mantissa_temp; //the product_o is 0
+                    exponent_temp = exponent_temp; //the exponent is 0
+
+                    signal_a = a_i[31]; //extract the sign bit from a_i
+                    signal_b = b_i[31]; //extract the sign bit from b_i
+
+                    exponent_a = a_i[30:23]; //extract the exponent from a_i
+                    exponent_b = b_i[30:23]; //extract the exponent from b_i
+
+                    mantissa_a[23] = 1;
+                    mantissa_a[22:0] = a_i[22:0];  //extract the mantissa from a_i adding the implict 1 at the beginning
+                    mantissa_b[23] = 1;
+                    mantissa_b[22:0] = b_i[22:0];  //extract the mantissa from b_i adding the implict 1 at the beginning
+
+                    if (mantissa_a[22-:23] == 0 || mantissa_b[22-:23] == 0) begin //if any of the mantissas is 0
+                        mantissa_temp = 0; //the product_o is 0
+                        exponent_temp = 0; //the exponent is 0
+                    end else begin
+                        mantissa_temp = mantissa_a * mantissa_b; //multiply the mantissa
+                        exponent_temp = exponent_a + exponent_b - 127; //add the exponents and subtract the bias
+
+                        if (mantissa_temp[47] == 1) begin //if the most significant bit of the product_o is 1
+                            mantissa_temp = mantissa_temp >> 1; //shift the product_o to the right
+                            exponent_temp = exponent_temp + 1; //increment the exponent
+                        end
+
+                    end
+
+                    next_state = RESULT_VERIFICATION_STEP;
+            end
+
+            RESULT_VERIFICATION_STEP:
+                is_multiplication_done = 1;
+                next_state = DONE;
+            DONE:
+                next_state = INITIAL_STATE;
+
+            default: 
+                next_state = INITIAL_STATE;
+        endcase
+    end
+
+
+end
+
+
+always_ff @(posedge clk, negedge rst_n) begin : MULTIPLIER_FF_BLOCK
+    if(!rst_n)
+        done_o <= 1'b0;
+        nan_o <= 1'b0;
+        inifinit_o <= 1'b0;
+        overflow_o <= 1'b0;
+        underflow_o <= 1'b0;
+        product_o <= 32'b0;
+    else begin
+
+        product_o[31] <= signal_a ^ signal_b; //XOR the sign bits to get the sign of the product_o
+        product_o[30:23] <=  exponent_temp[7:0]; //add the exponents and subtract the bias
+        product_o[22:0] <= mantissa_temp[45:23]; //extract the 24 most significant bits of the product_o
+
+        case (current_state)
+            INITIAL_STATE:
+                done_o <= 1'b0;
+                nan_o <= 1'b0;
+                inifinit_o <= 1'b0;
+                overflow_o <= 1'b0;
+                underflow_o <= 1'b0;
+                product_o <= 32'b0;
+
+            MULTIPLY_STEP:
+
+            RESULT_VERIFICATION_STEP:
+                if (product_o[30:23] == 8'hFF && product_o[22:0] != 23'b00) begin
+                    nan_o <= 1'b1;
+                    product_o <= 32'b00;
+                end else if (product_o[30:23] == 8'b11111111 && product_o[22:0] == 23'b0) begin
+                    inifinit_o <= 1'b1;
+                end else if (exponent_temp[8] == 1'b1 || exponent_temp[9] == 1'b1) begin
+                    overflow_o <= 1'b1;
+                    product_o <= 32'h7FFFFFFF;
+                end else if (product_o[30:23] == 8'b00000000) begin
+                    underflow_o <= 1'b1;
+                    product_o <= 32'b00;
+                end else begin
+                    done_o <= 1'b1;
+                end
+
+                done_o <= 1'b1;
+
+            DONE:
+                done_o <= 1'b0;
+        endcase
+    end
+end
+
+
+
+
+
+
+
+/*
 always_comb begin : blockName
-    mantissa_temp = mantissa_temp; //the result is 0
+    mantissa_temp = mantissa_temp; //the product_o is 0
     exponent_temp = exponent_temp; //the exponent is 0
 
-    signal_a = input_a[31]; //extract the sign bit from input_a
-    signal_b = input_b[31]; //extract the sign bit from input_b
+    signal_a = a_i[31]; //extract the sign bit from a_i
+    signal_b = b_i[31]; //extract the sign bit from b_i
 
-    exponent_a = input_a[30:23]; //extract the exponent from input_a
-    exponent_b = input_b[30:23]; //extract the exponent from input_b
+    exponent_a = a_i[30:23]; //extract the exponent from a_i
+    exponent_b = b_i[30:23]; //extract the exponent from b_i
 
     mantissa_a[23] = 1;
-    mantissa_a[22:0] = input_a[22:0];  //extract the mantissa from input_a adding the implict 1 at the beginning
+    mantissa_a[22:0] = a_i[22:0];  //extract the mantissa from a_i adding the implict 1 at the beginning
     mantissa_b[23] = 1;
-    mantissa_b[22:0] = input_b[22:0];  //extract the mantissa from input_b adding the implict 1 at the beginning
+    mantissa_b[22:0] = b_i[22:0];  //extract the mantissa from b_i adding the implict 1 at the beginning
 
     if (mantissa_a[22-:23] == 0 || mantissa_b[22-:23] == 0) begin //if any of the mantissas is 0
-        mantissa_temp = 0; //the result is 0
+        mantissa_temp = 0; //the product_o is 0
         exponent_temp = 0; //the exponent is 0
     end else begin
         mantissa_temp = mantissa_a * mantissa_b; //multiply the mantissa
         exponent_temp = exponent_a + exponent_b - 127; //add the exponents and subtract the bias
 
-        if (mantissa_temp[47] == 1) begin //if the most significant bit of the result is 1
-            mantissa_temp = mantissa_temp >> 1; //shift the result to the right
+        if (mantissa_temp[47] == 1) begin //if the most significant bit of the product_o is 1
+            mantissa_temp = mantissa_temp >> 1; //shift the product_o to the right
             exponent_temp = exponent_temp + 1; //increment the exponent
         end
 
     end
 
-    // result[31] = signal_a ^ signal_b; //XOR the sign bits to get the sign of the result
-    // result[30:23] =  exponent_temp[7:0]; //add the exponents and subtract the bias
-    // result[22:0] = mantissa_temp[45:23]; //extract the 24 most significant bits of the result
+    // product_o[31] = signal_a ^ signal_b; //XOR the sign bits to get the sign of the product_o
+    // product_o[30:23] =  exponent_temp[7:0]; //add the exponents and subtract the bias
+    // product_o[22:0] = mantissa_temp[45:23]; //extract the 24 most significant bits of the product_o
 
 end
 
 always_ff @(posedge clk, posedge reset) begin : clocked_block
 
     if (reset) begin
-        result <= 32'b0;
+        product_o <= 32'b0;
     end else begin
-        result[31] <= signal_a ^ signal_b; //XOR the sign bits to get the sign of the result
-        result[30:23] <=  exponent_temp[7:0]; //add the exponents and subtract the bias
-        result[22:0] <= mantissa_temp[45:23]; //extract the 24 most significant bits of the result
+        product_o[31] <= signal_a ^ signal_b; //XOR the sign bits to get the sign of the product_o
+        product_o[30:23] <=  exponent_temp[7:0]; //add the exponents and subtract the bias
+        product_o[22:0] <= mantissa_temp[45:23]; //extract the 24 most significant bits of the product_o
     end
 end
 
-
+*/
 
 
 
--- physical_synthesis/synthesis/frontend/ieee754multiplier_tb.sv
+++ physical_synthesis/synthesis/frontend/ieee754multiplier_tb.sv
@@ -2,20 +2,30 @@
 module multiplier_tb (
 );
 
-logic [31:0] input_a;
-logic [31:0] input_b;
-logic [31:0] result;
-logic clk, reset;
+logic clk, rst_n;
+logic [31:0] a_i;
+logic [31:0] b_i;
+logic start_i;
+logic done_o;
+logic nan_o;
+logic inifinit_o;
+logic overflow_o;
+logic underflow_o;
+logic [31:0] product_o;
 
-shortreal mock_a;
-shortreal mock_b;
 
 ieee754multiplier DUT (
-    .clk(clk),
-    .reset(reset),
-    .input_a(input_a),
-    .input_b(input_b),
-    .result(result)
+    .clk(clk), 
+    .rst_n(rst_n),
+    .a_i(a_i),
+    .b_i(b_i),
+    .start_i(start_i),
+    .done_o(done_o),
+    .nan_o(nan_o),
+    .inifinit_o(inifinit_o),
+    .overflow_o(overflow_o),
+    .underflow_o(underflow_o),
+    .product_o(product_o)
 );
 
 initial begin
@@ -29,46 +39,50 @@ end
 
 
 initial begin
+
+    rst_n = 0;
+    #10;
+    rst_n = 1;
     #10;
 
-    mock_a = 7.5;
-    mock_b = 2.2;
+    a_i = 7.5;
+    b_i = 2.2;
 
-    input_a = $shortrealtobits(mock_a);
-    input_b = $shortrealtobits(mock_b);
+    input_a = $shortrealtobits(a_i);
+    input_b = $shortrealtobits(b_i);
 
     #10;
-    $display("Result Bits: %b", result);
-    $display("Result: %f", $bitstoshortreal(result));
-    $display("Expected: %f", mock_a * mock_b);
+    $display("product_o Bits: %b", product_o);
+    $display("product_o: %f", $bitstoshortreal(product_o));
+    $display("Expected: %f", a_i * b_i);
 
     // $finish;
 
     #10;
 
-    mock_a = 3.1415;
-    mock_b = 0.792;
+    a_i = 3.1415;
+    b_i = 0.792;
 
-    input_a = $shortrealtobits(mock_a);
-    input_b = $shortrealtobits(mock_b);
-
-    #10;
-    $display("Result Bits: %b", result);
-    $display("Result: %f", $bitstoshortreal(result));
-    $display("Expected: %f", mock_a * mock_b);
+    input_a = $shortrealtobits(a_i);
+    input_b = $shortrealtobits(b_i);
 
     #10;
-
-    mock_a = 0.0;
-    mock_b = 3.1415;
-
-    input_a = $shortrealtobits(mock_a);
-    input_b = 32'b0_11111001_11100000000000000000000;//$shortrealtobits(mock_b);
+    $display("product_o Bits: %b", product_o);
+    $display("product_o: %f", $bitstoshortreal(product_o));
+    $display("Expected: %f", a_i * b_i);
 
     #10;
-    $display("Result Bits: %b", result);
-    $display("Result: %f", $bitstoshortreal(result));
-    $display("Expected: %f", mock_a * mock_b);
+
+    a_i = 0.0;
+    b_i = 3.1415;
+
+    input_a = $shortrealtobits(a_i);
+    input_b = 32'b0_11111001_11100000000000000000000;//$shortrealtobits(b_i);
+
+    #10;
+    $display("product_o Bits: %b", product_o);
+    $display("product_o: %f", $bitstoshortreal(product_o));
+    $display("Expected: %f", a_i * b_i);
 
     $finish;
 
