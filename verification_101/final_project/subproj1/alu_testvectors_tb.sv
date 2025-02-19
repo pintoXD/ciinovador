3dffd17c683fd4b16d008d6bdb5f7066d27a14b4
Otto Pinto <ottolopes20@gmail.com>
2025-02-19 14:26:42 -0300

    testvectors done

--- verification_101/final_project/subproj1/alu_testvectors_tb.sv
+++ verification_101/final_project/subproj1/alu_testvectors_tb.sv
@@ -12,7 +12,7 @@ module alu_testvectors_tb;
   logic expected_CarryOut;
   int num_vectors, i;
   logic [7:0] testvectors [0:119];
-  logic [15:0] vectornum, errors; // bookkeeping variables
+  logic [31:0] vectornum, errors; // bookkeeping variables
 
   // Instantiate the ALU
   alu uut (
@@ -25,25 +25,51 @@ module alu_testvectors_tb;
     .CarryOut(CarryOut)
   );
 
-    initial begin
-        clock = 0;
-        forever begin
-            #5 clock = ~clock;
-        end
+    always begin
+        clock = 1; #5; clock = 0; #5;
     end
-
 
     initial begin
         $readmemh("data2.tv", testvectors);
+        vectornum = 0;
+        errors = 0;
+        reset = 1;
+        #30;
+        reset = 0;
     end
+
+    always @(posedge clock)
+    begin
+        #1;
+        A = testvectors[vectornum];
+        B = testvectors[vectornum+1];
+        ALU_Sel = testvectors[vectornum+2];
         
-    initial begin
-        #100;
-        $display("Test vectors read from file:");
-        for(i=0; i<120; i=i+5) begin
-            // $display("A = %h, B = %h, ALU_Sel = %h", testvectors[i][7:0], testvectors[i][15:8], testvectors[i][31:16]);
-            $display("A = %h, B = %h, ALU_Sel = %h, Expected_ALU_Out=%h, Expected_CarryOut=%h", testvectors[i], testvectors[i+1], testvectors[i+2], testvectors[i+3], testvectors[i+4]);
+        #10; 
+        if (testvectors[vectornum] === 8'bxx || testvectors[vectornum + 1] === 8'bxx || testvectors[vectornum + 2] === 8'bxx) begin
+            $display("Finished testing all vectors. %d errors found.", errors);
+            $finish;
         end
+    end
+
+
+    always @(negedge clock) begin
+        #100;
+        
+        expected_ALU_Out = testvectors[vectornum+3];
+        expected_CarryOut = testvectors[vectornum+4];
+
+        if (ALU_Out == 8'bx || CarryOut == 1'bx) begin
+            $display("Finished testing all vectors. %d errors found.", errors);
+            $finish;
+        end
+        
+        if (ALU_Out !== expected_ALU_Out || CarryOut !== expected_CarryOut) begin
+            errors = errors + 1;
+            $display("Error in vector %d: A = %h, B = %h, ALU_Sel = %h, Expected_ALU_Out=%h, Expected_CarryOut=%h, ALU_Out=%h, CarryOut=%h", i/5, A, B, ALU_Sel, expected_ALU_Out, expected_CarryOut, ALU_Out, CarryOut);
+        end
+
+        vectornum = vectornum + 5;
     end
 
 
