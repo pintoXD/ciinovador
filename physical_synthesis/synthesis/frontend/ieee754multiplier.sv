d3a08cc10e5c4c155e85c70fbe227efc3f35b5e4
Otto Pinto <ottolopes20@gmail.com>
2025-02-24 09:45:16 -0300

    updated the done_o thing

--- physical_synthesis/synthesis/frontend/ieee754multiplier.sv
+++ physical_synthesis/synthesis/frontend/ieee754multiplier.sv
@@ -134,7 +134,9 @@ always_ff @(posedge clk, negedge rst_n) begin : MULTIPLIER_FF_BLOCK
                 underflow_o <= 1'b0;
                 product_o <= 32'b0;
             end
-            MULTIPLY_STEP:
+            MULTIPLY_STEP: begin
+                done_o <= 1'b1;
+            end
 
             RESULT_VERIFICATION_STEP: begin
                 if (product_o[30:23] == 8'hFF && product_o[22:0] != 23'b00) begin
@@ -152,7 +154,7 @@ always_ff @(posedge clk, negedge rst_n) begin : MULTIPLIER_FF_BLOCK
                     done_o <= 1'b1;
                 end
 
-                done_o <= 1'b1;
+                done_o <= 1'b0;
             end
 
             DONE:
@@ -160,62 +162,5 @@ always_ff @(posedge clk, negedge rst_n) begin : MULTIPLIER_FF_BLOCK
         endcase
     end
 end
-
-
-
-
-
-
-
-/*
-always_comb begin : blockName
-    mantissa_temp = mantissa_temp; //the product_o is 0
-    exponent_temp = exponent_temp; //the exponent is 0
-
-    signal_a = a_i[31]; //extract the sign bit from a_i
-    signal_b = b_i[31]; //extract the sign bit from b_i
-
-    exponent_a = a_i[30:23]; //extract the exponent from a_i
-    exponent_b = b_i[30:23]; //extract the exponent from b_i
-
-    mantissa_a[23] = 1;
-    mantissa_a[22:0] = a_i[22:0];  //extract the mantissa from a_i adding the implict 1 at the beginning
-    mantissa_b[23] = 1;
-    mantissa_b[22:0] = b_i[22:0];  //extract the mantissa from b_i adding the implict 1 at the beginning
-
-    if (mantissa_a[22-:23] == 0 || mantissa_b[22-:23] == 0) begin //if any of the mantissas is 0
-        mantissa_temp = 0; //the product_o is 0
-        exponent_temp = 0; //the exponent is 0
-    end else begin
-        mantissa_temp = mantissa_a * mantissa_b; //multiply the mantissa
-        exponent_temp = exponent_a + exponent_b - 127; //add the exponents and subtract the bias
-
-        if (mantissa_temp[47] == 1) begin //if the most significant bit of the product_o is 1
-            mantissa_temp = mantissa_temp >> 1; //shift the product_o to the right
-            exponent_temp = exponent_temp + 1; //increment the exponent
-        end
-
-    end
-
-    // product_o[31] = signal_a ^ signal_b; //XOR the sign bits to get the sign of the product_o
-    // product_o[30:23] =  exponent_temp[7:0]; //add the exponents and subtract the bias
-    // product_o[22:0] = mantissa_temp[45:23]; //extract the 24 most significant bits of the product_o
-
-end
-
-always_ff @(posedge clk, posedge reset) begin : clocked_block
-
-    if (reset) begin
-        product_o <= 32'b0;
-    end else begin
-        product_o[31] <= signal_a ^ signal_b; //XOR the sign bits to get the sign of the product_o
-        product_o[30:23] <=  exponent_temp[7:0]; //add the exponents and subtract the bias
-        product_o[22:0] <= mantissa_temp[45:23]; //extract the 24 most significant bits of the product_o
-    end
-end
-
-*/
-
-
 
 endmodule
